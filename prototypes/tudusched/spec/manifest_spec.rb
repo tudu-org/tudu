f8cad53a8272460c16b3bd9db22dc5172bfcf885
gerow <gerow.mike@gmail.com>
2013-10-21 15:05:33 -0700

    set up a ruby spec that fails

---
+++ prototypes/tudusched/spec/manifest_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Tudusched::Manifest do
+  before :each do
+    @manifest = Tudusched.load_manifest_file 'manifest/test_dispatch_one.json'
+  end
+
+  it "should return a Manifest object" do
+    @manifest.should be_an_instance_of Tudusched.Manifest
+  end
+end
---
+++ prototypes/tudusched/spec/spec_helper.rb
@@ -0,0 +1,2 @@
+require_relative '../lib/tudusched'
+require_relative '../lib/tudusched/manifest'
