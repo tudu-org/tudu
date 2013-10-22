d905f0a9449319cbd20748938261d337e9c5ff0e
gerow <gerow.mike@gmail.com>
2013-10-22 01:30:06 -0700

    we now have code to interact with the google api

--- prototypes/tudusched/lib/tudusched.rb
+++ prototypes/tudusched/lib/tudusched.rb
@@ -1,5 +1,6 @@
 require 'json'
 require 'optparse'
+require 'google/api_client'
 
 require 'tudusched/scheduler'
 require 'tudusched/manifest'
@@ -10,6 +11,25 @@ module Tudusched
       h = JSON.parse(IO.read(filename))
 
       Manifest.new h
+    end
+
+    def get_google_client
+      client = Google::APIClient.new
+
+      client.authorization.client_id = '1076895517677.apps.googleusercontent.com'
+      client.authorization.client_secret = 'GMQpEKkIGxF2lSVF1AqUOKNf'
+      client.authorization.redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
+
+      client.authorization.scope = 'https://www.googleapis.com/auth/calendar.readonly'
+
+      redirect_uri = client.authorization.authorization_uri
+
+      print "Please navigate your browser to <#{redirect_uri}> and paste your result in the terminal\n"
+      print "Result: "
+
+      client.authorization.code = gets.chomp
+
+      client
     end
 
     def cli
@@ -32,12 +52,21 @@ module Tudusched
           "print the free entries we generate from the given schedule") do
           options[:free_entries] = true
         end
+
+        opts.on("-g", "--google",
+          "use the google calendar API to get the schedule list") do
+          options[:google] = true
+        end
       end.parse!
 
       print "Using input file #{options[:input]}\n"
       print "Using output file #{options[:output]}\n"
 
       m = load_manifest_file options[:input]
+
+      if options[:google]
+        client = get_google_client
+      end
 
       if options[:free_entries]
         IO.write(options[:output], m.free_entries.map{|e| e.to_h}.to_json)
