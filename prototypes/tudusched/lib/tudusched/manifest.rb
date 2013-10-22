51633f6eb5472ec145ad8fd932913c9be3d833c6
gerow <gerow.mike@gmail.com>
2013-10-21 18:29:28 -0700

    added functions to allow us to determine a task's priority at a certain time

--- prototypes/tudusched/lib/tudusched/manifest.rb
+++ prototypes/tudusched/lib/tudusched/manifest.rb
@@ -12,10 +12,55 @@ module Tudusched
       @end_time = DateTime.parse(args['end'])
       @schedule = args['schedule'].map{|e|
         Tudusched::ScheduleEntry.new e
+      }.sort_by{|e|
+        e.start_time
       }
       @tasks = args['tasks'].map{|e|
         Tudusched::Task.new e
       }
+    end
+
+    def free_entries
+      cur_time = start_time
+      entries = []
+
+      schedule.each do |e|
+        if e.start_time == cur_time
+          next
+        end
+
+        entries << ScheduleEntry.new(:name => 'free',
+          :start_time => cur_time, :end_time => e.start_time)
+        cur_time = e.end_time
+      end
+
+      if cur_time != end_time
+        entries << ScheduleEntry.new(:name => 'free',
+          :start_time => cur_time, :end_time => end_time)
+      end
+
+      entries
+    end
+
+    def schedule_task
+      # schedule a single task, then return.
+      # We do this because after a task is scheduled
+      # we must recompute the free_entries list, and instead
+      # of doing this recursively we just call schedule_task
+      # over and over while there are tasks to be scheduled
+
+      tasks = tasks.sort_by{|e|
+        e.priority
+      }
+    end
+
+    def schedule_tasks
+      while not tasks.empty?
+        if not schedule_task
+          # if it returns false that means a task failed
+          # to be scheduled. We'll have to do something about it.
+        end
+      end
     end
   end
 end
--- prototypes/tudusched/lib/tudusched/task.rb
+++ prototypes/tudusched/lib/tudusched/task.rb
@@ -2,13 +2,56 @@ require 'date'
 
 module Tudusched
   class Task
-    attr_reader :name, :time, :priority, :due
+    attr_reader :name, :time, :importance, :due
+
+    def add_priority_fn &block
+      @priority_fns ||= []
+      @priority_fns << block
+    end
 
     def initialize args={}
       @name = args['name']
       @time = args['time']
-      @priority = args['priority']
+      @priority = args['importance']
       @due = DateTime.parse(args['due'])
+
+      add_priority_fn do |cur_time|
+        # funciton for "importance"
+        importance
+      end
+
+      add_priority_fn do |cur_time|
+        # function for "time-til due-"
+        time_til_due = due - cur_time
+        # time_til_due is in days, so we'll
+        # convert it to seconds first just so
+        # we can use the function from 
+        # the clojure implementation
+        time_til_due = time_til_due * 24 * 60 * 60
+        if time_til_due == 0
+          # if this is the case then this should have
+          # a very high priority.  How high? well for
+          # now we'll just return a very big number...
+          # this should be fixed in the future
+          next 100000000000
+        end
+
+        # otherwise, use our little formula
+        172800 / time_til_due * 4
+      end
+
+      add_priority_fn do |cur_time|
+        # function for "time-to-finish".
+        # currently has zero weight
+        0
+      end
+    end
+
+
+    def priority cur_time
+      @priority_fns.reduce(0){|memo, e|
+        memo + e.call(cur_time)
+      }
     end
   end
 end
