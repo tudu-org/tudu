79ccc47c3f7646ad1d8acc48a2728614358d3d09
gerow <gerow.mike@gmail.com>
2013-10-22 00:10:04 -0700

    rmoved unnecessary methods from tudusched

--- prototypes/tudusched/lib/tudusched.rb
+++ prototypes/tudusched/lib/tudusched.rb
@@ -6,14 +6,6 @@ require 'tudusched/manifest'
 
 module Tudusched
   class << self
-    def info
-      return "this is just a test!"
-    end
-
-    def schedule manifest
-
-    end
-
     def load_manifest_file filename
       h = JSON.parse(IO.read(filename))
 
