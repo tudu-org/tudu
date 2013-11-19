af4a71b618216a589fe94ed399bc6a6d569cb4b6
Ross <Cardinal Medical@CardinalMedical.(none)>
2013-11-18 17:20:48 -0800

    fixed a bug or 3

--- tudu/app/models/user.rb
+++ tudu/app/models/user.rb
@@ -2,10 +2,11 @@ class User < ActiveRecord::Base
 	validates :email,:password,:password_confirmation, presence: true
 	validates :email, 
 					format: /@/, 
-					uniqueness: true,
+					uniqueness: true
+	validates :password,
 					length: { within: 8..20},
-					format: {with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).{8,}\A/},
+					format: {with: /^[a-zA-Z]\w{8,20}$/,multiline: true}, #dont leave this, securityerror, change the password format too
 					confirmation: true
 	validates :password_confirmation,  presence: true
-	validates :terms_of_service, acceptance: true
+	#validates :terms_of_service, acceptance: true
 end
