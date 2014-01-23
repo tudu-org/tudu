6143df3b22efce0f325616cac5686bdd65a79e47
gerow <gerow.mike@gmail.com>
2014-01-22 17:43:50 -0800

    We now have a decent base system for starting development

--- puppet/modules/tudu-dev/manifests/init.pp
+++ puppet/modules/tudu-dev/manifests/init.pp
@@ -1,5 +1,25 @@
 class tudu-dev {
-  class {'tudu-dev::ruby2': }
+  exec {'apt-update':
+    command => '/usr/bin/apt-get update',
+  }
 
-  package {'tmux': ensure=> 'installed'}
+  package {'tmux':
+    ensure => 'installed',
+    require => Exec['apt-update'],
+  }
+  package {'ruby-dev':
+    ensure => 'installed',
+    require => Exec['apt-update'],
+  }
+
+  package {'bundler':
+    ensure => 'installed',
+    provider => 'gem',
+  }
+
+  package {'rails':
+    ensure => 'installed',
+    provider => 'gem',
+    require => Package['ruby-dev'],
+  }
 }
