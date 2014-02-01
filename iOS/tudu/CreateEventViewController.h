918e14fc46749038c809cef16d31abab52b5533d
Jonathan Rusnak <jonathanrusnak@guest-wireless-upc-nat-206-117-88-029.usc.edu>
2014-02-01 10:31:05 -0800

    Added Create Event View Controller

---
+++ iOS/tudu/CreateEventViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateEventViewController.h
+//  tuduBeta
+//
+//  Created by Jonathan Rusnak on 2/1/14.
+//  Copyright (c) 2014 Jonathan Rusnak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateEventViewController : UIViewController
+
+@end
