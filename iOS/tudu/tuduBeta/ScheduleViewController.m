b3d5a5a7dc622d5b166cf8100718b324bd44f32e
Jonathan Rusnak <jrusnak@usc.edu>
2014-02-17 13:18:19 -0800

    Added buttons to switch views

--- iOS/tudu/tuduBeta/ScheduleViewController.m
+++ iOS/tudu/tuduBeta/ScheduleViewController.m
@@ -11,6 +11,9 @@
 
 @interface ScheduleViewController ()
 @property (weak, nonatomic) IBOutlet UIView *calendarView;
+@property (weak, nonatomic) IBOutlet UIButton *dayView;
+@property (weak, nonatomic) IBOutlet UIButton *weekView;
+@property (weak, nonatomic) IBOutlet UIButton *monthView;
 
 @end
 
@@ -29,6 +32,12 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    //Set up buttons
+    
+    [self.dayView addTarget:self action:@selector(viewTypeButtonPushed:) forControlEvents:UIControlEventTouchUpInside];
+    [self.weekView addTarget:self action:@selector(viewTypeButtonPushed:) forControlEvents:UIControlEventTouchUpInside];
+    [self.monthView addTarget:self action:@selector(viewTypeButtonPushed:) forControlEvents:UIControlEventTouchUpInside];
+    
     // 1. Instantiate a CKCalendarView
     self.calendar = [CKCalendarView new];
     
@@ -36,7 +45,10 @@
     [self.calendar setDelegate:self];
     [self.calendar setDataSource:self];
     
-    // 3. Present the calendar
+    // 3. Set as Day View
+    self.calendar.displayMode = 2;//2 = Day View 0 = Month View 1 = Week View
+
+    // 4. Present the calendar
     [[self calendarView] addSubview:self.calendar];
 
 }
@@ -45,6 +57,22 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)viewTypeButtonPushed:(id)sender
+{
+    if([[sender currentTitle] isEqual:@"Month"]){
+        self.calendar.displayMode = 0;
+    }
+    
+    if([[sender currentTitle] isEqual:@"Week"]){
+        self.calendar.displayMode = 1;
+    }
+    
+    if([[sender currentTitle] isEqual:@"Day"]){
+        self.calendar.displayMode = 2;
+    }
+    
 }
 
 @end
