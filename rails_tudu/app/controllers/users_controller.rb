88ef5b2c50eee62c0ea269d1ac53100245e8aef4
gerow <gerow.mike@gmail.com>
2014-02-27 02:51:56 -0800

    we can now get user information by id

--- rails_tudu/app/controllers/users_controller.rb
+++ rails_tudu/app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html {
+
+      }
+      format.json {
+        render json: @user
+      }
+    end
+  end
+
   def by_email
     @user = User.find_by email: params[:email]
 
