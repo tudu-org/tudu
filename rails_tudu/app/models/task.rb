9bf18c39cfc47b158da3b879d76ac4110cda389c
gerow <gerow.mike@gmail.com>
2014-02-27 02:03:43 -0800

    made it to where a user has_many tasks

--- rails_tudu/app/models/task.rb
+++ rails_tudu/app/models/task.rb
@@ -1,2 +1,3 @@
 class Task < ActiveRecord::Base
+  attr_accessible :start_time, :end_time, :name, :description, :priority, :deadline
 end
--- rails_tudu/db/schema.rb
+++ rails_tudu/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140227093505) do
+ActiveRecord::Schema.define(version: 20140227100018) do
 
   create_table "events", force: true do |t|
     t.datetime "start_time"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20140227093505) do
     t.datetime "deadline"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
