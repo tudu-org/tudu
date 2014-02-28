ae5b38e0b4b8f24982cf161c016353254bf6979f
gerow <gerow.mike@gmail.com>
2014-02-27 20:17:11 -0800

    removed the url fields from the json for events and tasks
    (they were wrong anyway)

--- rails_tudu/app/controllers/users_controller.rb
+++ rails_tudu/app/controllers/users_controller.rb
@@ -24,4 +24,32 @@ class UsersController < ApplicationController
       }
     end
   end
+
+  def create
+    @user = User.new(params[:user])
+
+    if @user.valid?
+      @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = 'Welcome'
+    end
+
+    respond_to do |format|
+      if @user.valid?
+        format.html {
+
+        }
+        format.json {
+          render json: @user
+        }
+      else
+        format.html {
+
+        }
+        format.json {
+          render json: @user, status: 400
+        }
+      end
+    end
+  end
 end
