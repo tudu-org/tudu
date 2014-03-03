0efa634a056765eeb2c8bdee9b2135b3e1822b6a
gerow <gerow.mike@gmail.com>
2014-03-02 21:17:50 -0800

    task scheduling is kinda integrated... still need to provide controllers for it
    Also, it might actually be kinda broken :(

--- rails_tudu/db/schema.rb
+++ rails_tudu/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140302113933) do
+ActiveRecord::Schema.define(version: 20140303043946) do
 
   create_table "events", force: true do |t|
     t.datetime "start_time"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20140302113933) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "duration"
   end
 
   create_table "users", force: true do |t|
